8481e32efab1aeae8f154f1f408ab8449002df28
Student <example@example.com>
2020-01-31 15:11:35 -0800

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :homes do
     get '/current_temps' => 'homes#latest_temps_by_home'
     resources :rooms do
+      get '/ac_on?' => 'rooms#ac_on?'
+      get '/heater_on?' => 'rooms#heater_on?'
+      patch '/update_setting' => 'rooms#configure_temp_setting'
       get '/current_temp' => 'rooms#latest_temp_by_room'
       resource :temperatures
     end
