7c121d2aabe116d98ec80a109644d0418fd0b31b
Student <example@example.com>
2020-01-31 09:27:49 -0800

    test commit

--- db/migrate/20200131172457_create_temperatures.rb
+++ db/migrate/20200131172457_create_temperatures.rb
@@ -1,6 +1,9 @@
 class CreateTemperatures < ActiveRecord::Migration[5.2]
   def change
     create_table :temperatures do |t|
+      t.int :temperature
+      
+      t.timestamps
     end
   end
 end
